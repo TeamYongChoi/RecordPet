5fee5eb288b3bcc4e2201b8e82faffc27d35a934
jinsung <sanoi12@naver.com>
2025-01-23 22:13:20 +0900

    feat : 건강 기록 등록 예외처리 #40
    - 애완동물이 유효한 값인지 확인

--- src/main/java/teamyc/recordpet/domain/healthrecord/service/HealthRecordService.java
+++ src/main/java/teamyc/recordpet/domain/healthrecord/service/HealthRecordService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import teamyc.recordpet.domain.healthrecord.dto.HealthRecordCreateRequest;
 import teamyc.recordpet.domain.healthrecord.repository.HealthRecordRepository;
 import teamyc.recordpet.domain.pet.repository.PetRepository;
+import teamyc.recordpet.global.exception.GlobalException;
+import teamyc.recordpet.global.exception.ResultCode;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +16,11 @@ public class HealthRecordService {
     private final PetRepository petRepository;
 
     public void save(Long petId, HealthRecordCreateRequest request) {
-        //TODO 유효한 펫 아이디인지 검증하는 구문 작성
-        // weightLog 관련 내용 작성 되면 작성하기
-        // petRepository.(petId);
+
+        if(!petRepository.existsById(petId)){
+            throw new GlobalException(ResultCode.NOT_FOUND_PET_PROFILE);
+        }
+
         healthRecordRepository.save(request.toEntity());
     }
 }
