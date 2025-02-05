ba2787e44a275930eb0df25cef209d5a70e3a16c
SOHEE-YONG <yongcowhee@gmail.com>
2025-02-06 01:53:19 +0900

    test: User Profile 편집 테스트 성공 코드 추가

--- src/test/java/teamyc/recordpet/base/UserTest.java
+++ src/test/java/teamyc/recordpet/base/UserTest.java
@@ -1,29 +1,37 @@
 package teamyc.recordpet.base;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import teamyc.recordpet.domain.user.entity.User;
 
 public class UserTest {
-
-    PasswordEncoder passwordEncoder;
 
     protected Long TEST_USER_ID = 1L;
     protected String TEST_USER_NAME = "username";
     String TEST_USER_EMAIL = "username@gmail.com";
     protected String TEST_USER_PASSWORD = "@Abce4!3024821";
     protected String TEST_USER_NEXT_PASSWORD = "!@#Asflsds234";
+    protected String TEST_USER_PROFILE_IMAGE = "test_image_url";
+    protected String TEST_UPDATED_USER_PROFILE_IMAGE = "test_updated_image_url";
 
     protected User TEST_USER = User.builder()
         .userId(TEST_USER_ID)
         .nickname(TEST_USER_NAME)
         .email(TEST_USER_EMAIL)
         .password(TEST_USER_PASSWORD)
+        .userProfileImageUrl(TEST_USER_PROFILE_IMAGE)
         .build();
 
-    protected User TEST_CHANGE_USER = User.builder()
+    protected User TEST_UPDATED_USER = User.builder()
         .userId(TEST_USER_ID)
         .nickname(TEST_USER_NAME)
         .email(TEST_USER_EMAIL)
         .password(TEST_USER_NEXT_PASSWORD)
+        .userProfileImageUrl(TEST_UPDATED_USER_PROFILE_IMAGE)
+        .build();
+
+    protected User TEST_NO_PROFILE_IMAGE_USER = User.builder()
+        .userId(TEST_USER_ID)
+        .nickname(TEST_USER_NAME)
+        .email(TEST_USER_EMAIL)
+        .password(TEST_USER_PASSWORD)
         .build();
 }
--- src/test/java/teamyc/recordpet/domain/user/service/UserServiceTest.java
+++ src/test/java/teamyc/recordpet/domain/user/service/UserServiceTest.java
@@ -1,8 +1,11 @@
 package teamyc.recordpet.domain.user.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
@@ -15,11 +18,14 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import teamyc.recordpet.base.UserTest;
 import teamyc.recordpet.domain.user.dto.UserChangePasswordRequest;
+import teamyc.recordpet.domain.user.dto.UserEditProfileRequest;
 import teamyc.recordpet.domain.user.entity.User;
 import teamyc.recordpet.domain.user.repository.UserRepository;
+import teamyc.recordpet.global.s3.S3Service;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest extends UserTest {
@@ -32,6 +38,9 @@ class UserServiceTest extends UserTest {
     PasswordEncoder passwordEncoder;
     @Captor
     ArgumentCaptor<User> argumentCaptor;
+
+    @Mock
+    S3Service s3Service;
 
     @Nested
     @DisplayName("비밀번호 변경 테스트")
@@ -46,11 +55,11 @@ class UserServiceTest extends UserTest {
 
             given(userRepository.findByUserId(any(Long.class))).willReturn(
                 Optional.ofNullable(TEST_USER));
-            given(userRepository.save(any(User.class))).willReturn(TEST_CHANGE_USER);
+            given(userRepository.save(any(User.class))).willReturn(TEST_UPDATED_USER);
             given(passwordEncoder.matches(req.getCurrentPassword(), TEST_USER_PASSWORD)).willReturn(
                 true);
             given(passwordEncoder.encode(TEST_USER_NEXT_PASSWORD)).willReturn(
-                TEST_CHANGE_USER.getPassword());
+                TEST_UPDATED_USER.getPassword());
 
             // when
             userService.changePassword(TEST_USER_ID, req);
@@ -59,8 +68,82 @@ class UserServiceTest extends UserTest {
             verify(userRepository).save(any(User.class));
             verify(userRepository).findByUserId(any(Long.class));
             verify(userRepository).save(argumentCaptor.capture());
-            assertEquals(TEST_USER_NEXT_PASSWORD, TEST_CHANGE_USER.getPassword());
+            assertEquals(TEST_USER_NEXT_PASSWORD, TEST_UPDATED_USER.getPassword());
             assertEquals(TEST_USER_ID, argumentCaptor.getValue().getUserId());
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자 프로필 편집 테스트")
+    class editUserProfile {
+
+        @Test
+        @DisplayName("사용자 프로필 편집 성공-multipartFile이 비어있는 경우")
+        void editUserProfileSuccess_EmptyMultipartFile() {
+            // given
+            UserEditProfileRequest req = UserEditProfileRequest.builder()
+                .nickname(TEST_USER_NAME)
+                .build();
+
+            given(userRepository.findByUserId(anyLong())).willReturn(
+                Optional.ofNullable(TEST_USER));
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("image", "", "",
+                new byte[0]);
+
+            // when
+            userService.editProfile(TEST_USER_ID, req, mockMultipartFile);
+
+            // then
+            verify(userRepository).findByUserId(TEST_USER_ID);
+            verify(userRepository).save(any());
+        }
+
+        @Test
+        @DisplayName("사용자 프로필 편집 성공-multipartFile 업로드-기존 프로필 있는 경우")
+        void editUserProfileSuccess_NotEmptyMultipartFile_ExistProfileImage() {
+            // given
+            UserEditProfileRequest req = UserEditProfileRequest.builder()
+                .nickname(TEST_USER_NAME)
+                .build();
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("image", "profile.jpg",
+                "image/jpeg", "fake image".getBytes());
+
+            given(userRepository.findByUserId(anyLong())).willReturn(
+                Optional.ofNullable(TEST_USER));
+            given(s3Service.uploadImage(any(), any())).willReturn(anyString());
+
+            // when
+            userService.editProfile(TEST_USER_ID, req, mockMultipartFile);
+
+            // then
+            verify(userRepository).findByUserId(TEST_USER_ID);
+            verify(userRepository).save(any());
+            verify(s3Service).deleteFile(any());
+            verify(s3Service).uploadImage(mockMultipartFile, "user-profile-images");
+        }
+
+        @Test
+        @DisplayName("사용자 프로필 편집 성공-multipartFile 업로드-기존 프로필 없는 경우")
+        void editUserProfileSuccess_NotEmptyMultipartFile_NoExistProfileImage() {
+            // given
+            UserEditProfileRequest req = UserEditProfileRequest.builder()
+                .nickname(TEST_USER_NAME)
+                .build();
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("image", "profile.jpg",
+                "image/jpeg", "fake image".getBytes());
+
+            given(userRepository.findByUserId(anyLong())).willReturn(
+                Optional.ofNullable(TEST_NO_PROFILE_IMAGE_USER));
+            given(s3Service.uploadImage(any(), any())).willReturn(anyString());
+
+            // when
+            userService.editProfile(TEST_USER_ID, req, mockMultipartFile);
+
+            // then
+            verify(userRepository).findByUserId(TEST_USER_ID);
+            verify(userRepository).save(any());
+            verify(s3Service, never()).deleteFile(any());
+            verify(s3Service).uploadImage(mockMultipartFile, "user-profile-images");
         }
     }
 }
