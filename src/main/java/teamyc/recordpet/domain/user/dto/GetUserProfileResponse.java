deddb05446667ba5528aa33e8b8950e404f567fe
SOHEE-YONG <yongcowhee@gmail.com>
2025-02-07 20:33:28 +0900

    refactor: User 프로필 조회 dto 메서드 수정

--- src/main/java/teamyc/recordpet/domain/user/dto/GetUserProfileResponse.java
+++ src/main/java/teamyc/recordpet/domain/user/dto/GetUserProfileResponse.java
@@ -2,6 +2,7 @@ package teamyc.recordpet.domain.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import teamyc.recordpet.domain.user.entity.User;
 
 @Getter
 public class GetUserProfileResponse {
@@ -13,5 +14,13 @@ public class GetUserProfileResponse {
     public GetUserProfileResponse(String nickname, String profileImageUrl) {
         this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
+    }
+
+    public static GetUserProfileResponse fromEntity(User user) {
+        String imageUrl = user.getProfileUrl();
+        return GetUserProfileResponse.builder()
+            .nickname(user.getNickname())
+            .profileImageUrl(imageUrl)
+            .build();
     }
 }
