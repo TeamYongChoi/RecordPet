7d83745630aa465152d06197567276e6d6f09fa7
SOHEE-YONG <yongcowhee@gmail.com>
2025-02-12 16:09:44 +0900

    feat: 기본 프로필 이미지 Optional 처리

--- src/main/java/teamyc/recordpet/global/image/ProfileImageRepository.java
+++ src/main/java/teamyc/recordpet/global/image/ProfileImageRepository.java
@@ -1,6 +1,7 @@
 package teamyc.recordpet.global.image;
 
 import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 import teamyc.recordpet.global.image.entity.ProfileImage;
@@ -10,7 +11,7 @@ import teamyc.recordpet.global.image.entity.Type;
 public interface ProfileImageRepository {
 
     @Query(value = "select p from ProfileImage p where p.isBasic = true AND p.type = :type")
-    ProfileImage findBasicImage(@Param("type") Type type);
+    Optional<ProfileImage> findBasicImage(@Param("type") Type type);
 
     ProfileImage save(ProfileImage profileImage);
 
